560cbc997271cdd8d6964f42fb4b5f4e9bfc6a87
Developer <Developer@email.com>
2024-10-12 10:50:52 +0200

    created header component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,17 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { DashboardModule } from './pages/dashboard/dashboard.module';
-import { MatToolbarModule } from '@angular/material/toolbar';
+import { HeaderComponent } from './common/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, DashboardModule, RouterOutlet],
+  imports: [
+    CommonModule, 
+    DashboardModule, 
+    HeaderComponent,
+    RouterOutlet
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
