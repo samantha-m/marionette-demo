f29576cb2a27d60d18cfb7a6ab9ba29d066c883e
Developer <Developer@email.com>
2024-10-12 10:56:53 +0200

    created footer component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { DashboardModule } from './pages/dashboard/dashboard.module';
 import { HeaderComponent } from './common/header/header.component';
+import { FooterComponent } from './common/footer/footer.component';
 
 @Component({
   selector: 'app-root',
@@ -11,6 +12,7 @@ import { HeaderComponent } from './common/header/header.component';
     CommonModule, 
     DashboardModule, 
     HeaderComponent,
+    FooterComponent,
     RouterOutlet
   ],
   templateUrl: './app.component.html',
